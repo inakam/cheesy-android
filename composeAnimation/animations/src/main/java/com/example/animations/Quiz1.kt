deccbf3b73d3245ab7d7bbd5a74a74433b1e2d48
inakam <nakamura.ibuki@nifty.co.jp>
2023-08-22 17:50:30 +0900

    [Quiz1] ボタンのアニメーションを修正して追加

--- composeAnimation/animations/src/main/java/com/example/animations/Quiz1.kt
+++ composeAnimation/animations/src/main/java/com/example/animations/Quiz1.kt
@@ -13,23 +13,29 @@ import androidx.compose.ui.graphics.graphicsLayer
 @Composable
 fun RotatingButton() {
     // 回転の状態を管理する変数
-    var rotationState by remember { mutableStateOf(0f) }
+    var continuousRotation by remember { mutableStateOf(0f) }
     // アニメーションが進行中かどうかを管理する変数
     var isAnimating by remember { mutableStateOf(false) }
 
-    val rotation by animateFloatAsState(
-        targetValue = if (isAnimating) 360f else 0f,
-        animationSpec = if (isAnimating) {
-            // 加速しながら開始、減速しながら終了
-            keyframes {
-                durationMillis = 4000 // アニメーションの合計時間
-                0f at 0 with FastOutSlowInEasing // 開始時の回転角度
-                360f at 4000 with FastOutSlowInEasing // 終了時の回転角度
-            }
-        } else {
-            spring() // 通常はspringアニメーション
-        }
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val rotation by infiniteTransition.animateFloat(
+        initialValue = continuousRotation,
+        targetValue = continuousRotation + 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 4000,
+                easing = FastOutSlowInEasing
+            ),
+            repeatMode = RepeatMode.Restart
+        )
     )
+
+    LaunchedEffect(isAnimating) {
+        if (isAnimating) {
+            continuousRotation += 360f
+        }
+    }
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -37,10 +43,10 @@ fun RotatingButton() {
     ) {
         Button(
             onClick = {
-                isAnimating = !isAnimating // アニメーション状態をトグル
+                isAnimating = !isAnimating // アニメーション状態を切り替える
             },
             modifier = Modifier.graphicsLayer(
-                rotationZ = rotation // 回転角度を適用
+                rotationZ = if (isAnimating) rotation else continuousRotation // 回転角度を適用
             )
         ) {
             Text("Rotate Me!")
